197d095a5f5c230089e617f52aebd9ce787bc83c
roger <estag.rogercmj@pge.pr.gov.br>
2024-07-08 15:31:07 -0300

    Criando componentes

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <PrimeiroComponenteVue/>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+  import PrimeiroComponenteVue from "./components/PrimeiroComponente.vue";
+  export default {
+    name: "App",
+    components: {
+      PrimeiroComponenteVue,
+    },
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
