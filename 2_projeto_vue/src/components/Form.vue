d29a3d9f399df0e949ea88c915ab7ff5bebe5734
roger <estag.rogercmj@pge.pr.gov.br>
2024-07-09 16:47:24 -0300

    Eventos (@submit e @click)

--- 2_projeto_vue/src/components/Form.vue
+++ 2_projeto_vue/src/components/Form.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
         <h2>Meu Formulário:</h2>
-        <form action="">
+        <form action="" @submit="enviarFormulario($event)">
             <div>
-                <InputText/>
+                <input type="text" v-model="name">
             </div>
             <div>
-                <InputText/>
+                <input type="text" v-model="email">
             </div>
             <div>
                 <Submit/>
             </div>
         </form>
+
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
     </div>
 </template>
 
@@ -24,6 +28,31 @@ export default {
     components: {
         InputText,
         Submit
+    },
+    data() {
+        return {
+            name: '',
+            email: ''
+        }
+    },
+    methods: {
+        enviarFormulario(e) {
+            e.preventDefault();
+
+            const name = this.name;
+            const email = this.email;
+
+            console.log('Formulário enviado com sucesso!')
+
+            console.log("O nome é: " + name);
+            console.log("O email é: " + email);
+        }
     }
 }
 </script>
+
+<style scoped>
+p {
+    margin: 150px;
+}
+</style>
