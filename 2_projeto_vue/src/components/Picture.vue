1246093c739d35dc4a47cabc55dd0c80ebc5f999
roger <estag.rogercmj@pge.pr.gov.br>
2024-07-12 15:42:11 -0300

    Emit

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
         <img :src="avatar" :alt="descricao">
-        <Reutilizacao />
+        <MudarImagem @mudar-imagem="mudarImagem" />
     </div>
 </template>
 
 <script>
 import Reutilizacao from './Reutilizacao.vue';
+import MudarImagem from './MudarImagem.vue';
 
 export default {
     name: 'Picture',
     components: {
-        Reutilizacao
+        Reutilizacao,
+        MudarImagem
+    },
+    methods: {
+        mudarImagem() {
+            this.avatar = '/img/avatar2.png'
+        }
     },
     data() {
         return {
